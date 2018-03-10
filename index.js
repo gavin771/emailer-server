3cea63eff8eb5fce6532cff188b71499fd598619
gavin771 <gavin.samuels10@gmail.com>
2018-03-10 12:09:38 -0800

    added basic google passport auth

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 const app = express();
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+const gcid = process.env.GoogleClientID;
+const gcs = process.env.GoogleClientSecret;
+
+passport.use(new GoogleStrategy({
+  clientID: gcid,
+  clientSecret: gcs,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+}))
+
+app.get('/', passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(process.env.PORT || 5000);
