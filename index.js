760b2b3c5b5ff69faf8ddef8e6c86addf6113ffc
gavin771 <gavin.samuels10@gmail.com>
2018-03-10 16:18:21 -0800

    Added database and passport oauth flow

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 require('dotenv').config();
+
+
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds211029.mlab.com:11029/${process.env.MONGO_DB}`)
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./models/User');
 require('./services/passport');
 
 //Routes
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,33 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('users');
 const gcid = process.env.GoogleClientID;
 const gcs = process.env.GoogleClientSecret;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
   clientID: gcid,
   clientSecret: gcs,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
+  User.findOne({ googleId: profile.id }).then((user) => {
+    if (user) {
+      done(null, user);
+    } else {
+      new User({ googleId: profile.id })
+        .save()
+        .then(user => done(null, user))
+    }
+  })
 }));
