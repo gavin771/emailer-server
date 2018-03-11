11f269693057899d6b0191c89080446398d66ca1
gavin771 <gavin.samuels10@gmail.com>
2018-03-11 12:22:13 -0700

    added fetchUser to workflow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import * as actions from '../actions'
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
+  componentDidMount () {
+    this.props.fetchUser();
+  }
+
   render () {
     return (
       <div className="App">
@@ -24,4 +28,5 @@ class App extends Component {
   }
 }
 
-export default App;
+
+export default connect(null, actions)(App);
