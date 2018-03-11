14babd5a6a8925ab394484e099be6c0e03db2e11
gavin771 <gavin.samuels10@gmail.com>
2018-03-11 10:07:12 -0700

    setup auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(() => [], {}, applyMiddleware())
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware())
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
