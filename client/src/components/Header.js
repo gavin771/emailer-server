e13c906b921e8e4205a088cc07e01fac17299cd2
gavin771 <gavin.samuels10@gmail.com>
2018-03-11 17:00:29 -0700

    wired up auth workflow with header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent (auth) {
+    console.log(auth);
+    switch (auth) {
+      case null:
+        return null;//()
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        )
+      default:
+        return (
+          <li>
+            <a href="/auth/logout">Logout</a>
+          </li>
+        )
+    }
+  }
   render () {
     return (
       <nav>
         <div className="container nav-wrapper">
-          <a href="/" className="brand-logo">Emaily</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/auth/google">Login with Google</a></li>
+          <a href="/" className="brand-logo left">Emaily</a>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent(this.props.auth)}
+
           </ul>
         </div>
       </nav>
@@ -15,4 +36,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps ({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, null)(Header)
--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,13 @@ const passport = require('passport');
 module.exports = (app) => {
   app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/surveys')
+  });
 
-  app.get('/api/logout', (req, res) => {
+  app.get('/auth/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/')
   });
 
   app.get('/api/me', (req, res) => {
