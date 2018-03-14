0c59947821218c5d0a82ae3a22561abac7939bed
gavin771 <gavin.samuels10@gmail.com>
2018-03-13 17:48:48 -0700

    added credits label to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,10 @@ class Header extends Component {
       default:
         return [
           <li key='1'><Payments /></li>,
-          <li key='2'>
+          <li key='2' style={{ margin: '0 15px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key='3'>
             <a href="/auth/logout">Logout</a>
           </li>
         ];
