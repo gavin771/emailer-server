607b626062aa69e24c491ced4dbb8ee1c9bc7b0f
gavin771 <gavin.samuels10@gmail.com>
2018-03-13 19:03:34 -0700

    added travis && production serving code

--- index.js
+++ index.js
@@ -25,4 +25,15 @@ require('./services/passport');
 require('./routes/auth')(app);
 require('./routes/api')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //look in client/build for file that match unknown requests
+  app.use(express.static('client/build'));
+
+  //for all other routes, serve the index.html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.res(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(process.env.PORT || 5000);
