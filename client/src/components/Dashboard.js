82d56c066920939dd8abc3a11a970768978c6586
gavin771 <gavin.samuels10@gmail.com>
2018-03-15 18:42:55 -0700

    added dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div class="fixed-action-btn">
+        <Link to="/surveys/new" class="btn waves-effect waves-light btn-floating btn-large red">
+          <i class="large material-icons">add</i>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
