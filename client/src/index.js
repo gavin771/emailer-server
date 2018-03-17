7e58788e90fac3e32605bad7904bc1ac3ccfc827
gavin771 <gavin.samuels10@gmail.com>
2018-03-17 05:15:08 -0700

    updated env

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk';
 import 'materialize-css/dist/css/materialize.min.css'
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers'
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-//registerServiceWorker();
+registerServiceWorker();
+
+console.log(process.env.REACT_APP_STRIPE_KEY)
