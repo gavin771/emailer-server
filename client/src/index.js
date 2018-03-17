4a01291be2cc6a7f987804e956f8800639d3f37a
gavin771 <gavin.samuels10@gmail.com>
2018-03-17 00:53:58 -0700

    removed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,10 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk';
 import 'materialize-css/dist/css/materialize.min.css'
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers'
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
