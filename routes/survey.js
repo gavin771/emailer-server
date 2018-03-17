e7032676d99127cf010c766ac304c05269fa6c11
gavin771 <gavin.samuels10@gmail.com>
2018-03-16 23:04:20 -0700

    parsed webhook data

--- routes/survey.js
+++ routes/survey.js
@@ -5,6 +5,9 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
 const keys = require('../config/keys');
 const Mailer = require('../services/Mailer')
 const Survey = mongoose.model('surveys');
+const Path = require('path-parser');
+const { URL } = require('url');
+const _ = require('lodash');
 
 module.exports = (app) => {
   app.post('/api/surveys', [isLoggedIn, hasCredits], async (req, res) => {
@@ -28,6 +31,37 @@ module.exports = (app) => {
     } catch (err) {
       res.status(422).send(err);
     }
+  })
+
+  app.get('/api/surveys/webhooks', (req, res) => {
+    req.body = [
+      { event: 'click', url: "http://host.com/api/surveys/8907/yes", choice: "yes", surveyId: "7890" },
+      { event: 'click', url: "http://host.com/api/surveys/8907/yes", choice: "yes", surveyId: "7890" },
+      { event: 'bounce', url: "http://host.com/api/surveys/8907/yes", choice: "yes", surveyId: "7890" },
+      { event: 'click', url: "http://host.com/api/surveys/", choice: "yes", surveyId: "7890" },
+
+    ]
+
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          }
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+
+    console.log(events);
+
+    res.send();
   })
 
   app.get('/api/surveys/thanks', (req, res) => {
